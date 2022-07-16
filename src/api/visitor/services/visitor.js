3613919926f6de2fd2aab7fd6739a7591d879e5d
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-07-16 00:56:25 +0000

    removed key

--- src/api/visitor/services/visitor.js
+++ src/api/visitor/services/visitor.js
@@ -1,49 +1,45 @@
-'use strict';
+"use strict";
 
 /**
  * visitor service.
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::visitor.visitor', ({ strapi }) => ({
+module.exports = createCoreService("api::visitor.visitor", ({ strapi }) => ({
+  // Method 2: Wrapping a core action (leaves core logic in place)
+  async find(ctx) {
+    // some custom logic here
+    ctx.query = { ...ctx.query, local: "en" };
 
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
 
-    // Method 2: Wrapping a core action (leaves core logic in place)
-    async find(ctx) {
-        // some custom logic here
-        ctx.query = { ...ctx.query, local: 'en' }
+    const API_KEY = process.env.MAILGUN_API_KEY;
+    const DOMAIN = process.env.MAILGUN_DOMAIN;
 
-        // Calling the default core action
-        const { data, meta } = await super.find(ctx);
+    const formData = require("form-data");
+    const Mailgun = require("mailgun.js");
 
-        console.log(data, "hi");
-        const API_KEY = '22930a2b5747ff11389adeff30614708-7dcc6512-0c20f774';
-        const DOMAIN = 'paakofiaidoo.tech';
+    const mailgun = new Mailgun(formData);
+    const client = mailgun.client({ username: "api", key: API_KEY });
 
-        const formData = require('form-data');
-        const Mailgun = require('mailgun.js');
+    const messageData = {
+      from: "Excited User <me@samples.mailgun.org>",
+      to: "paakofiaidoo17@gmail.com",
+      subject: "Hello",
+      text: "Testing some Mailgun awesomeness!",
+    };
 
-        const mailgun = new Mailgun(formData);
-        const client = mailgun.client({ username: 'api', key: API_KEY });
+    client.messages
+      .create(DOMAIN, messageData)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
 
-        const messageData = {
-            from: 'Excited User <me@samples.mailgun.org>',
-            to: 'paakofiaidoo17@gmail.com',
-            subject: 'Hello',
-            text: 'Testing some Mailgun awesomeness!'
-        };
-
-        client.messages.create(DOMAIN, messageData)
-            .then((res) => {
-                console.log(res);
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-        data.location.hi = "hello"
-
-        return { data, meta };
-    },
-
+    return { data, meta };
+  },
 }));
